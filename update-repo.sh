fa53c04435828b3e06145b35178af3963017dcfd
Doris Hsu <dhsu@nvidia.com>
2021-10-12 10:19:43 +0800

    update helm repo content

---
+++ update-repo.sh
@@ -0,0 +1,5 @@
+cd ~/Desktop/egx-repo/doris-hsu.github.io/
+helm repo index charts/ --url https://doris-hsu.github.io/charts/
+git add .
+git commit -m "update helm repo content"
+git push -u origin master
